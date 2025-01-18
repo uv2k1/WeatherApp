85735e95df5e1c2c07be43db452266874c609e01
Yuvraj Soni <yuvrajsoni2k1@gmail.com>
2025-01-18 17:56:11 +0530

    project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import WeatherApp from './Componants/WeatherApp/WeatherApp'
+
+
+function App() {
+  
+  return (
+    <>
+      <WeatherApp />
+    </>
+  )
+}
+
+export default App
---
+++ src/Componants/abc.jsx
@@ -0,0 +1,4 @@
+export default function abc(){
+
+     return <div>abc</div>
+}
